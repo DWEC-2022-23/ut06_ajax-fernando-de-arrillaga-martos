748304598a6b9f7881c5c1ba3675b13617544852
fernandodearrillaga <115628468+fernandodearrillaga@users.noreply.github.com>
2023-02-17 23:30:32 +0100

    GET, POST y PUT en el checkbox
    GET, POST y PUT en el checkbox completados

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
+var obj;
+var obj1;
+
+async function conexionAjax() {
+  // await code here
+  let result = await makeRequest("GET", "http://localhost:3000/invitados");
+  // code below here will only execute when await makeRequest() finished loading
+  console.log(result);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+  conexionAjax();
   const form = document.getElementById('registrar');
   const input = form.querySelector('input');
   
@@ -62,6 +73,27 @@ document.addEventListener('DOMContentLoaded', () => {
     input.value = '';
     const li = createLI(text);
     ul.appendChild(li);
+    console.log("añadido");
+    console.log(obj1.length-1);
+    let postObj = { 
+      id: obj1[obj1.length-1].id+1, 
+      nombre: text, 
+      confirmado: false
+  }
+
+    let post = JSON.stringify(postObj)
+    const url = "http://localhost:3000/invitados"
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    xhr.send(post);
+    xhr.onload = function () {
+      if(xhr.status === 201) {
+          console.log("Post successfully created!") 
+      }
+  }
+   
+    
   });
     
   ul.addEventListener('change', (e) => {
@@ -71,8 +103,71 @@ document.addEventListener('DOMContentLoaded', () => {
     
     if (checked) {
       listItem.className = 'responded';
+      console.log(event.target.parentNode.parentNode);
+      console.log(listItem.getElementsByTagName("span")[0]);
+      console.log(obj1);
+      for (let i = 0; i < document.getElementsByTagName("li").length; i++) {
+        console.log(document.getElementsByTagName("li")[i].getElementsByTagName("span")[0]);
+        if(document.getElementsByTagName("li")[i].getElementsByTagName("span")[0]==listItem.getElementsByTagName("span")[0]){
+          console.log(i);
+          console.log(obj1[i].id);
+
+          let putObj = { 
+            id: obj1[i].id, 
+            nombre: obj1[i].nombre, 
+            confirmado: true
+        }
+      
+          let put = JSON.stringify(putObj);
+          const url = "http://localhost:3000/invitados/"+obj1[i].id;
+          let xhr = new XMLHttpRequest();
+          xhr.open('PUT', url, true);
+          xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+          xhr.send(put);
+          xhr.onload = function () {
+            if(xhr.status === 200) {
+                console.log("Put successfull!") 
+            }
+        }
+        }
+        
+        
+      }
+      //console.log(getElementsByTagName("span")[i])
+      
+      
+
+
     } else {
       listItem.className = '';
+
+      for (let i = 0; i < document.getElementsByTagName("li").length; i++) {
+        console.log(document.getElementsByTagName("li")[i].getElementsByTagName("span")[0]);
+        if(document.getElementsByTagName("li")[i].getElementsByTagName("span")[0]==listItem.getElementsByTagName("span")[0]){
+          console.log(i);
+          console.log(obj1[i].id);
+
+          let putObj = { 
+            id: obj1[i].id, 
+            nombre: obj1[i].nombre, 
+            confirmado: false
+        }
+      
+          let put = JSON.stringify(putObj);
+          const url = "http://localhost:3000/invitados/"+obj1[i].id;
+          let xhr = new XMLHttpRequest();
+          xhr.open('PUT', url, true);
+          xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+          xhr.send(put);
+          xhr.onload = function () {
+            if(xhr.status === 200) {
+                console.log("Put successfull!") 
+            }
+        }
+        }
+        
+        
+      }
     }
   });
     
@@ -112,7 +207,81 @@ document.addEventListener('DOMContentLoaded', () => {
 });  
   
   
-  
+function makeRequest(method, url) {
+  return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("invitedList").innerHTML = this.responseText;
+           obj=this.responseText;
+           obj1 = JSON.parse(obj);
+          console.log(obj1);
+          for (let i = 0; i < obj1.length; i++) {
+            
+            const li = document.createElement("li");
+          const span = document.createElement("span");
+          let nombre = document.createTextNode(obj1[i].nombre);
+          let label = document.createElement("label");
+          const confirmed = document.createTextNode("Confirmed");
+          const confirmedCheckbox= document.createElement("input");
+          confirmedCheckbox.type= "checkbox";
+          const buttonEdit = document.createElement("button");
+          const buttonRemove = document.createElement("button");
+          console.log(obj1[i].confirmado);
+          if (obj1[i].confirmado==true) {
+            confirmedCheckbox.checked = true;
+            console.log("responded");
+            li.className="responded";
+            
+          }
+
+          buttonEdit.appendChild(document.createTextNode("edit"));
+          buttonRemove.appendChild(document.createTextNode("remove"));
+
+          
+          label.appendChild(confirmed);
+          label.appendChild(confirmedCheckbox);
+
+          span.appendChild(nombre);
+          
+          li.appendChild(span);
+          li.appendChild(label);
+          li.appendChild(buttonEdit);
+          li.appendChild(buttonRemove);
+
+          document.getElementById("invitedList").appendChild(li);
+          a0 = obj1.a0;
+          console.log(obj);
+          console.log(obj1);
+          console.log(obj1[i].confirmado);
+          console.log(obj1[i].nombre);
+          }
+      }
+      };
+      xhr.onerror = function () {
+          reject({
+              status: this.status,
+              statusText: xhr.statusText
+          });
+      };
+      xhr.send();
+  });
+}
+
+function appendJSON(){
+  const fs = require('fs');
+
+const content = 'Some content!33';
+
+fs.writeFile('texto.txt', content, err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
+});
+
+}
   
   
   
